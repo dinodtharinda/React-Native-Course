c617b75ae2fb158609a7b86c2738e683148a6d0f
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-07-22 10:45:49 +0530

    step - 2

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StyleSheet, View, Text } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-   <StartGameScreen/>
+    <LinearGradient colors={[ "#35011B","#FFC400"]} style={styles.rootScreen}>
+      <StartGameScreen />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center"
-  }
+  rootScreen: {
+    flex: 1,
+  },
 });
