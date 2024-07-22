efabc83e36d27c9264f54f5f4e8848715fb3af43
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-07-22 12:09:36 +0530

    title components

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,30 @@
-import { Text } from "react-native";
-function GameScreen(){
-    return <Text>Game Screen!</Text>
+import { Text, View, StyleSheet,SafeAreaView } from "react-native";
+import Title from "../components/Title";
+function GameScreen() {
+  return (
+    <View style={styles.screen}>
+     <Title>Opponenet's Guess</Title>
+      <View>
+        <Text>Higher or lower?</Text>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+  title:{
+    fontSize:24,
+    fontWeight:'bold',
+    color:"#ddb52f",
+    textAlign:'center',
+    borderWidth:2,
+    borderColor:"#ddb52f",
+    padding:12
+  }
+});
 
 export default GameScreen;
