296f83f7b8be7db7b0d04976b6207c345164f786
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-07-23 13:56:41 +0530

    ui changes with icons

--- components/ui/InstructionText.js
+++ components/ui/InstructionText.js
@@ -1,8 +1,8 @@
 import { Text, StyleSheet } from "react-native";
 import Colors from "../../constants/colors";
 
-function InstructionText({ children }) {
-  return <Text style={styles.instructionText}>{children}</Text>;
+function InstructionText({ children, style }) {
+  return <Text style={[, style,styles.instructionText]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
