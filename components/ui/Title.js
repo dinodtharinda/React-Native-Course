aff29382de0498fcd46a0307c0dfa78a84cce22e
Dinod Tharinda <94352564+dinodtharinda@users.noreply.github.com>
2024-07-29 19:58:48 +0530

    finish

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet, Dimensions } from "react-native";
+import { Text, StyleSheet, Dimensions ,Platform } from "react-native";
 
 
 function Title({ children }) {
@@ -12,7 +12,8 @@ const styles = StyleSheet.create({
     // fontWeight: "bold",
     color:'white',
     textAlign: "center",
-    borderWidth: 2,
+    // borderWidth: Platform.OS === 'android'? 3:0,
+    borderWidth:Platform.select({ios:4, android:2}),
     borderColor:'white',
     padding: 12,
     maxWidth:'80%',
